187f31b18ed21398b2711e6a4cc048a7a92c982e
Jeevitha <jeevitha.pugazh@gmail.com>
2026-03-12 14:44:26 -0400

    added render server url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+  import.meta.env.VITE_API_BASE_URL || "https://smart-notes-backend-mue6.onrender.com";
 
 function makeFormData(file) {
   const form = new FormData();
